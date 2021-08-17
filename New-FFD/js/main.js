6a27222cce51a2869af63769de5f2d4a38f3aadb
unknown <adnantanveer038@gmail.com>
2021-08-18 02:05:47 +0530

    asd

---
+++ New-FFD/js/main.js
@@ -0,0 +1,16 @@
+(function($) {
+
+    var form = $("#signup-form");
+    form.steps({
+        headerTag: "h3",
+        bodyTag: "fieldset",
+        transitionEffect: "fade",
+        labels: {
+            previous: 'Prev',
+            next: 'Next',
+            finish: 'Finish',
+            current: ''
+        },
+        titleTemplate: '<h3 class="title">#title#</h3>',
+    });
+})(jQuery);
